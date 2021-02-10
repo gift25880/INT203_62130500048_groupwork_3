066590d1942174b5018169e129a6c8508c73ced1
Pathinya <gift_01210@hotmail.com>
2021-02-10 11:40:47 +0700

    [UPDATE] add styles, element, event

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const app = {
     data() {
         return {
-            msg: 'Hello, Vue3'
+            gallery: [
+                { img: "./images/strawberry-sundae.png", menuTitle: "Strawberry Banana & Cream", click: false },
+                { img: "./images/coit-tower.png", menuTitle: "Coit Tower", click: false },
+                { img: "./images/banana-split.png", menuTitle: "Banana Split", click: false }
+            ]
+        }
+    },
+    methods: {
+        imgClicked(index) {
+            this.gallery[index].click = !this.gallery[index].click;
+        }
+    },
+    computed: {
+        photoAmount() {
+            return this.gallery.length;
         }
     }
 }
