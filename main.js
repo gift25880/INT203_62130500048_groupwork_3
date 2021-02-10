ab7e1f6c0f97a0843ea543738e1d30994d080d56
Pathinya <gift_01210@hotmail.com>
2021-02-10 12:13:01 +0700

    [UPDATE] add likes count

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = {
                 { img: "./images/strawberry-sundae.png", menuTitle: "Strawberry Banana & Cream", click: false },
                 { img: "./images/coit-tower.png", menuTitle: "Coit Tower", click: false },
                 { img: "./images/banana-split.png", menuTitle: "Banana Split", click: false }
-            ]
+            ],
         }
     },
     methods: {
@@ -16,6 +16,9 @@ const app = {
     computed: {
         photoAmount() {
             return this.gallery.length;
+        },
+        likeAmount() {
+            return this.gallery.filter(n => n.click).length;
         }
     }
 }
